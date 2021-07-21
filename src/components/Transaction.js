85f0aa5e82a53134d11176386d8479cb4d1dab49
Sandrine Poissonnet <spoisson@gmail.com>
2021-07-20 18:46:38 -0700

    app complete

---
+++ src/components/Transaction.js
@@ -0,0 +1,23 @@
+import React, { useContext } from 'react';
+
+import { GlobalContext } from '../context/GlobalState';
+
+export const Transaction = ({ transaction }) => {
+  const { deleteTransaction } = useContext(GlobalContext);
+  return (
+    <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
+      {transaction.text}{' '}
+      <span>
+        {transaction.amount < 0
+          ? `- $${Math.abs(transaction.amount)}`
+          : `+ $${transaction.amount}`}
+      </span>
+      <button
+        className="delete-btn"
+        onClick={() => deleteTransaction(transaction.id)}
+      >
+        x
+      </button>
+    </li>
+  );
+};
